146472f51ee9ecd34f83278711d9b4512724a942
ripator <ripatoroyan@gmail.com>
2022-09-22 15:18:25 +0400

    [FEAT]: Create C's simple printf function with string and number variables.

---
+++ simple-c-printf/simple-c-printf.js
@@ -0,0 +1,13 @@
+const printVariables = {
+   number: '%d',
+   string: '%s',
+};
+
+const simpleCPrintf = (inputString, ...characters) => {
+   characters.forEach((char) => {
+      inputString = inputString.replace(printVariables[typeof char], char);
+   });
+   console.log(inputString);
+}
+
+simpleCPrintf("ASCII value = %d, Charachter = %s, number = %d, etc.", 'string', 15, 31);
