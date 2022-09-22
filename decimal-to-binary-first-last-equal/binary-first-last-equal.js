bfbe3fc868820ed8db5569bf9c7b57603550ea89
ripator <ripatoroyan@gmail.com>
2022-09-22 12:47:27 +0400

    [FEAT]: Using only binary operations check if number's binary's first and last bits are the same.

--- decimal-to-binary-first-last-equal/binary-first-last-equal.js
+++ decimal-to-binary-first-last-equal/binary-first-last-equal.js
@@ -1,27 +1,8 @@
-const decimalToBinaryString = (value) => {
-   if (typeof value !== 'number' || Number.isNaN(value)) {
-      throw "Error: Value should be a number."
-   }
-   if (value >= 0) {
-      let binaryValue = (value).toString(2);
-      while (binaryValue.length < 32) {
-         binaryValue = '0' + binaryValue;
-      }
-      return binaryValue;
-   } else {
-      return (value >>> 0).toString(2);
-   }
+const areBinaryFirstLastBitsEqual = (value) => {
+   const lastBit = (value >>> 31);
+   const firstBit = ((value << 31) >>> 31);
+   return Boolean(lastBit & firstBit);
 }
 
-const areStringFirstAndLastElementsEqual = (value) => {
-   return value[0] === value[value.length - 1];
-}
-
-const isNumberBinaryFirstLastEqual = (value) => {
-   return areStringFirstAndLastElementsEqual(decimalToBinaryString(value));
-}
-
-console.log(decimalToBinaryString(-5)); // 11111111111111111111111111111011
-console.log(isNumberBinaryFirstLastEqual(-5)); // true
-console.log(decimalToBinaryString(15)); // 00000000000000000000000000001111
-console.log(isNumberBinaryFirstLastEqual(15)); // false
+console.log(areBinaryFirstLastBitsEqual(15)); // false --- 00000000000000000000000000001111
+console.log(areBinaryFirstLastBitsEqual(-5)); // true --- 11111111111111111111111111111011
---
+++ decimal-to-binary-first-last-equal/binary-string-first-last-equal.js
@@ -0,0 +1,27 @@
+const decimalToBinaryString = (value) => {
+   if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw "Error: Value should be a number."
+   }
+   if (value >= 0) {
+      let binaryValue = (value >>> 0).toString(2);
+      // while (binaryValue.length < 32) {
+      //    binaryValue = '0' + binaryValue;
+      // }
+      return binaryValue; 
+   } else {
+      return (value >>> 0).toString(2);
+   }
+}
+
+const areStringFirstAndLastElementsEqual = (value) => {
+   return value[0] === value[value.length - 1];
+}
+
+const isNumberBinaryFirstLastEqual = (value) => {
+   return areStringFirstAndLastElementsEqual(decimalToBinaryString(value));
+}
+
+console.log(decimalToBinaryString(-5)); // 11111111111111111111111111111011
+console.log(isNumberBinaryFirstLastEqual(-5)); // true
+console.log(decimalToBinaryString(15)); // 00000000000000000000000000001111
+console.log(isNumberBinaryFirstLastEqual(15)); // false
