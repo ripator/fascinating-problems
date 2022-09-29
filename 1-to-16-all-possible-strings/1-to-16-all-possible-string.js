7cd76def4a6c3c8a4acb4652c8db683cf1fa535d
ripator <ripatoroyan@gmail.com>
2022-09-29 21:52:27 +0400

    [FEAT]: Create 1-16 all possibe strings generator with english letters and 0-9 digits.

---
+++ 1-to-16-all-possible-strings/1-to-16-all-possible-string.js
@@ -0,0 +1,58 @@
+const generateCharsMap = (chars) => {
+   const charsMap = new Map();
+
+   charsMap.set('-1', '');
+
+   chars.forEach((char, index) => {
+      charsMap.set(`${index}`, char);
+   });
+
+   return charsMap;
+}
+
+const generateArrayWithGivenValue = (arrayLength, value) => {
+   let array = [];
+   for (let i = 0; i < arrayLength; i++) {
+      array.push(value);
+   }
+   return array;
+}
+
+const decodeMapValues = (map, array) => {
+   let mutatedArray = array.slice();
+   for (let i = 0; i < mutatedArray.length; i++) {
+      if (map.has(String(mutatedArray[i]))) {
+         mutatedArray[i] = map.get(String(mutatedArray[i]));
+      }
+   }
+   return mutatedArray.join('');
+}
+
+const incrementArrayValues = (array, lastKey) => {
+   for (let i = array.length-1; i >= 0; i--) {
+      if (array[i] != parseInt(lastKey)) {
+         array[i]++;
+         return; 
+      } else {
+         array[i] = 0;
+      }
+   }
+}
+
+const upto16AllPossibleStrings = () => {
+   const englishAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+   const decimals = '0123456789';
+   const emptyValue = -1;
+   const allCharsArray = decimals.concat(englishAlphabet).split('');
+   
+   const allCharsMap = generateCharsMap(allCharsArray);
+   let array16Symbols = generateArrayWithGivenValue(16, emptyValue);
+   const lastKeyInMap = [...allCharsMap.keys()].pop();
+
+   for (let i = 0; i < Math.pow(16, allCharsArray.length) - 1; i++) {
+      incrementArrayValues(array16Symbols, lastKeyInMap);
+      console.log(decodeMapValues(allCharsMap, array16Symbols));
+   }
+}
+
+upto16AllPossibleStrings();
