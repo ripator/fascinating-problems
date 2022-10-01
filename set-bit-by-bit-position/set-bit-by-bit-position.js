fd630971be15329165b23c2354763b47ef2084f3
ripator <ripatoroyan@gmail.com>
2022-10-01 19:00:40 +0400

    [FEAT]: Implement set bit by position method.

---
+++ set-bit-by-bit-position/set-bit-by-bit-position.js
@@ -0,0 +1,20 @@
+const set1ToPosition = (position) => {
+   return 1 << position // same as 2 ** position;
+}
+
+// sets 0 to bit
+const resetBitByPosition = (num, position) => {
+   const mask = set1ToPosition(position);
+   return mask & num;
+}
+
+//sets 1 to bit
+const setBitByPosition = (num, position) => {
+   const mask = set1ToPosition(position);
+   return mask | num;
+}
+
+const setBit = (num, position, reset = false) => {
+   const mask = set1ToPosition(position);
+   return reset ? (num & mask) : (num | mask);
+}
