cca0b717cab2edbd77648f46b5985bf61c780af5
ripator <ripatoroyan@gmail.com>
2022-10-19 12:38:50 +0400

    [FEAT]: Make resize array function type generic.

--- cpp/resize_array.cpp
+++ cpp/resize_array.cpp
@@ -2,16 +2,17 @@
 
 using namespace std;
 
-int* expandArr (int*& arr, int size, int new_size) {
+template<typename T>
+T* expandArr (T*& arr, int size, int new_size) {
    if (new_size > 0) {
-   int* new_arr = new int[new_size];
-
-   for(int i = 0; i < size; ++i) {
-      new_arr[i] = arr[i];
-   }
+      T* new_arr = new T[new_size];
    
-   delete [] arr;
-   arr = new_arr;
+      for(int i = 0; i < size; ++i) {
+         new_arr[i] = arr[i];
+      }
+      
+      delete [] arr;
+      arr = new_arr;
    }
    
    return arr;
@@ -19,13 +20,19 @@ int* expandArr (int*& arr, int size, int new_size) {
 
 
 int main() {
-   int* arr = new int[3];
+   int* arr = new int[3] {1, 2, 3};
    cout<<arr<<"\n";
    
    expandArr(arr, 3, 5);
    arr[5] = 10;
    cout<<arr<<"\n"; //different address
    
-   cout<<arr[5]; //10
+   cout<<arr[2]<<"\n"; // 3
+   cout<<arr[5]<<"\n"; //10
+   
+   char* arr1 = new char[2] {'H', 'e'};
+   expandArr(arr1, 2, 3);
+   arr1[2] = 'y';
+   cout<<arr1[2]; // y
    return 0;
 }
